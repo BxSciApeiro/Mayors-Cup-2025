bea4c3e96f4606ff133dd1a838c4576d26adeb47
EpicCarlito <jameschef2018@gmail.com>
2025-02-07 17:57:29 -0500

    WIP: tail movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/robotFunctions/tail.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/robotFunctions/tail.java
@@ -7,9 +7,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class tail {
     private final HardwareMap hwMap;
-    private final double y = 0.25; // left
-    private final double z = 0.75; // right
     private ElapsedTime timer;
+    private boolean goReturn = false;
 
     public tail(HardwareMap hwMap) {
         this.hwMap = hwMap;
@@ -19,15 +18,16 @@ public class tail {
         CRServo tail = hwMap.get(CRServo.class, "tail");
 
         if (gamepad.share) {
+            timer = new ElapsedTime();
+        }
+
+        if (timer != null && timer.seconds() > 1.0) {
+            goReturn = !goReturn;
             timer.reset();
-            while (timer.seconds()<= 5) {
-                if (timer.seconds() == 1.0 || timer.seconds() == 3.0 || timer.seconds() == 5.0) {
-                    tail.setPower(-0.05);
-                } else if (timer.seconds() == 2.0 || timer.seconds() == 4.0) {
-                    tail.setPower(0.05);
-                }
-            }
-    }
+        }
+
+        double statement = goReturn ? 0.15 : -0.15;
+        tail.setPower(statement);
     }
 }
 
