e4606a662087a7c1e86cb92d40689aff2c6dea1b
EpicCarlito <jameschef2018@gmail.com>
2025-02-10 16:19:55 -0500

    fixed goofy tail code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/robotFunctions/tail.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/robotFunctions/tail.java
@@ -7,15 +7,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class tail {
     private final HardwareMap hwMap;
-    private final double y = 0.25; // left
-    private final double z = 0.75; // right
     private ElapsedTime timer;
-<<<<<<< HEAD
     private boolean goReturn = false;
     private final Gamepad prevGamePad = new Gamepad();
     private boolean tailState = false;
-=======
->>>>>>> parent of bea4c3e (WIP: tail movement)
 
     public tail(HardwareMap hwMap) {
         this.hwMap = hwMap;
@@ -25,7 +20,6 @@ public class tail {
         CRServo tail = hwMap.get(CRServo.class, "tail");
 
         if (gamepad.share) {
-<<<<<<< HEAD
             if (!prevGamePad.share) {
                 tailState = !tailState;
             }
@@ -42,17 +36,6 @@ public class tail {
         } else {
             timer = null;
         }
-=======
-            timer.reset();
-            while (timer.seconds()<= 5) {
-                if (timer.seconds() == 1.0 || timer.seconds() == 3.0 || timer.seconds() == 5.0) {
-                    tail.setPower(-0.05);
-                } else if (timer.seconds() == 2.0 || timer.seconds() == 4.0) {
-                    tail.setPower(0.05);
-                }
-            }
-    }
->>>>>>> parent of bea4c3e (WIP: tail movement)
     }
 }
 
