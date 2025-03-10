8f58bc591aab23b8e963ac8758dfb8a14eb79984
EpicCarlito <jameschef2018@gmail.com>
2025-03-10 17:59:19 -0400

    happy customer of dairy 😁🥛

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/pedroTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/pedroTest.java
@@ -3,14 +3,13 @@ package org.firstinspires.ftc.teamcode.auton;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
 import com.pedropathing.pathgen.BezierCurve;
-import com.pedropathing.pathgen.BezierLine;
 import com.pedropathing.pathgen.Path;
 import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
 import com.pedropathing.util.Constants;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
